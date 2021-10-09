ca06d27c753677380610cda54b8f0f57bfa29c32
heojungeun <heocube@naver.com>
2021-10-09 20:59:04 +0900

    study event property

--- ch02/ch02_2/App.tsx
+++ ch02/ch02_2/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import {SafeAreaView, ScrollView} from 'react-native'
-import Person from './src/screens/Person'
-import * as D from './src/data'
+import {SafeAreaView, Alert, Button} from 'react-native'
+import {TouchableOpacity, TouchableHighlight, Text} from 'react-native'
+import { TextInput } from 'react-native'
+const onPress = () => Alert.alert('home pressed.', 'message')
 
-const people = D.makeArray(100).map(D.createRandomPerson)
 export default function App() {
-  const children = people.map((person) => (
-    <Person key={person.id} person={person} />
-  ))
   return (
     <SafeAreaView>
-      <ScrollView>{children}</ScrollView>
+      <Button title='Home' onPress={onPress} />
+      <Text onPress={onPress}>This Text has onPress function</Text>
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
     </SafeAreaView>
   )
 }
