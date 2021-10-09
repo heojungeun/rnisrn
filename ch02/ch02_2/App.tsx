2ed6cf5e114c1101d0e5364ef0bf9cb88eb3e409
heojungeun <heocube@naver.com>
2021-10-09 22:17:50 +0900

    study css platform, dimensions

--- ch02/ch02_2/App.tsx
+++ ch02/ch02_2/App.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
-import {StyleSheet, SafeAreaView, Text} from 'react-native'
+import {Platform, Dimensions, StyleSheet, SafeAreaView, Text, View} from 'react-native'
 import {Colors} from 'react-native-paper'
 import Color from 'color'
 
-console.log(Colors.blue500)
-console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string())
-
+const {width, height} = Dimensions.get('window')
+// prettier-ignore
 export default function App() {
   return (
     <SafeAreaView style={[styles.SafeAreaView]}>
-      <Text style={[styles.text]}>This Text has onPress function</Text>
+      <Text style={[styles.text]}>os: {Platform.OS}</Text>
+      <Text style={[styles.text]}>width: {width}</Text>
+      <Text style={[styles.text]}>height: {height}</Text>
+
+      <View style={[styles.box, styles.border]} />
+      <View style={[styles.box, styles.border, {borderRadius: 20}]} />
+      <View style={[styles.box, styles.border, {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]}
+      />
     </SafeAreaView>
   )
 }
 // prettier-ignore
 const styles = StyleSheet.create({
   SafeAreaView: {
-    backgroundColor: Colors.blue500,
+    backgroundColor: Colors.blue300,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
   text: {
     fontSize: 20,
-    color: Color(Colors.blue500).alpha(0.7).lighten(0.9).string(),
+    color: Color(Colors.blue500).lighten(0.9).string(),
+    marginBottom: 10,
+  },
+  box: {
+    height: 100, backgroundColor: Colors.lime500, marginBottom: 10,
+    marginLeft: Platform.select({ios: 20, android: 0}),
+  },
+  border: {
+    borderWidth: 10,
+    borderColor: Color('black').alpha(0.3).string(),
   },
 })
