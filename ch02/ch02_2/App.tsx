bef0d9af92c579a97940ab24ef941aea39d4b059
heojungeun <heocube@naver.com>
2021-10-18 21:22:28 +0900

    component flex, position

--- ch02/ch02_2/App.tsx
+++ ch02/ch02_2/App.tsx
@@ -1,32 +1,25 @@
 import React from 'react'
 // prettier-ignore
-import {StyleSheet, SafeAreaView, Platform, ImageBackground, Image, View, Text, Alert} from 'react-native'
-import {Colors} from 'react-native-paper'
+import {StyleSheet, SafeAreaView, Platform, View, Alert} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import * as D from './src/data'
-
-const avatarUrl = D.randomAvatarUrl()
-const avatarSize = 50
-const text = `Almost before we knew it, we had left the ground`
-const onIconPressed = () => Alert.alert('icon pressed')
+import {Colors} from 'react-native-paper'
+import TopBar from './src/screens/TopBar'
+import Content from './src/screens/Content'
+import BottomBar from './src/screens/BottomBar'
 
 export default function App() {
+  const iconPressed = () => Alert.alert('icon pressed')
   return (
-    <SafeAreaView style={[styles.flex]}>
-      <ImageBackground 
-        style={[styles.imageBackground]} 
-        source={require('./src/assets/images/bg.jpg')}>
-        <Image source={{uri: avatarUrl}} style={[styles.image]} />
-        <View style={[styles.flex, styles.padding10]}>
-          <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
-          <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
-          <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
-          <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
-        </View>
-        <Icon name="home" size={50} color={Colors.lightBlue500}
-        onPress={onIconPressed} />
-      </ImageBackground>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={[styles.flex]}>
+        <TopBar />
+        <Content />
+        <BottomBar />
+      </SafeAreaView>
+      <View style={[styles.absoluteView]}>
+        <Icon name="feather" size={50} color="white" onPress={iconPressed} />
+      </View>
+    </>
   )
 }
 // prettier-ignore
@@ -34,16 +27,19 @@ const styles = StyleSheet.create({
   flex: {flex: 1},
   imageBackground: {flex: 1},
   padding10: {padding: 10},
-  image: {
-    width: avatarSize,
-    height: avatarSize,
-    borderRadius: avatarSize / 2,
-  },
   text: {
     textAlign: 'center',
     fontSize: 25,
     color: 'black',
     marginBottom: 10,
+  },
+  absoluteView: {
+    backgroundColor: Colors.purple100,
+    position: 'absolute',
+    right: 30,
+    bottom: Platform.select({ios: 100, android: 80}),
+    padding: 10,
+    borderRadius: 35,
   },
 
   regular: {
