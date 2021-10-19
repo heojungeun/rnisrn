9f91d7d5fdfdac7356b548e32c4bf746949d071f
heojungeun <heocube@naver.com>
2021-10-19 23:18:09 +0900

    재사용가능한 touchableView, avatar component 생성

--- ch02/ch02_2/src/copy/Person.tsx
+++ ch02/ch02_2/src/copy/Person.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import type {FC} from 'react'
-import {Text, View, Image} from 'react-native'
+import {Text, View, Image, Alert} from 'react-native'
 import * as D from '../data'
 import {styles} from './Person.style'
 import moment from 'moment-with-locales-es6'
+import {Colors} from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {Avatar, IconText} from '../components'
 
 moment.locale('ko')
 
@@ -11,27 +14,44 @@ export type PersonProps = {
   person: D.IPerson
 }
 
+const avatarPressed = () => Alert.alert('avatar pressed.')
+const deletePressed = () => Alert.alert('delete pressed.')
+const countIconPressed = (name: string) => () => Alert.alert(`${name} pressed.`)
+
 // prettier-ignore
 const Person: FC<PersonProps> = ({person}) => {
   return (
     <View style={[styles.view]}>
-        <Image source={{uri: person.avatar}} style={[styles.avatar]} />
-        <View style={[styles.nameEmailView]}>
-            <Text style={[styles.name]}>{person.name}</Text>
-            <Text style={[styles.email]}>{person.email}</Text>
-        </View>
+      <View style={[styles.leftView]}>
+        <Avatar imageStyle={[styles.avatar]} uri={person.avatar} size={50} onPress={avatarPressed} />
+      </View>
+      <View style={[styles.rightView]}>
+        <Text style={[styles.name]}>{person.name}</Text>
+        <Text style={[styles.email]}>{person.email}</Text>
         <View style={[styles.dateView]}>
-            <Text style={[styles.createdDate]}>
+            <Text style={[styles.text]}>
                 {moment(person.createdDate).startOf('day').fromNow()}
             </Text>
+            <Icon name='trash-can-outline' size={26} color={Colors.lightBlue500} onPress={deletePressed} />
         </View>
-        <Text style={[styles.text]}>{person.comments}</Text>
+        <Text numberOfLines={3} ellipsizeMode="tail" style={[styles.text,
+          styles.comments]}>{person.comments}</Text>
         <Image style={[styles.image]} source={{uri: person.image}} />
         <View style={[styles.countsView]}>
-            <Text style={[styles.counts]}>{person.counts.comment}</Text>
-            <Text style={[styles.counts]}>{person.counts.retweet}</Text>
-            <Text style={[styles.counts]}>{person.counts.heart}</Text>
+            <IconText viewStyle={[styles.touchableIcon]}
+                  onPress={countIconPressed('comment')}
+                  name="comment" size={24} color={Colors.blue500}
+                  textStyle={[styles.iconText]}>{person.counts.comment}</IconText>
+            <IconText viewStyle={[styles.touchableIcon]}
+                  onPress={countIconPressed('retweet')}
+                  name="twitter-retweet" size={24} color={Colors.purple500}
+                  textStyle={[styles.iconText]}>{person.counts.retweet}</IconText>
+            <IconText viewStyle={[styles.touchableIcon]}
+                  onPress={countIconPressed('heart')}
+                  name="heart" size={24} color={Colors.red500}
+                  textStyle={[styles.iconText]}>{person.counts.heart}</IconText>
         </View>
+      </View>
     </View>
   )
 }
