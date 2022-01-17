3c7230e03a27ff9b2d91190c3eb08177afd3be2e
jung <heocube@naver.com>
2022-01-17 09:41:18 +0900

    update socket bk-fr connect

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,1 +1,3 @@
-alert("hi");
+//import WebSocket = require("ws");
+// frontsocket 서버와의 연결
+const frontsocket = new WebSocket(`ws://${window.location.host}`);
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -8,13 +8,19 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-//app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 //app.listen(3000, handleListen); 
 const server = http.createServer(app);
-
 const wss = new WebSocketServer({server}); 
 
-server.listen(3000, handleListen);
+// bksocket 연결된 브라우저 
+function handleConnection(bksocket){
+    console.log(bksocket);
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen); 
